3570982f937c7cd673a0b4d63bfe28af37ffdc44
Idan Nahum <idanahum1122@gmail.com>
2026-03-06 11:08:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 export const API_URL = "https://script.google.com/macros/s/AKfycbwP0huvvA5gXhSU-dtYwRimZPzqdOjTPWysgVWa0axZxsJaEwFye_MJdedLUTc9zSRx/exec";
 
 
-// ימים שמוצגים בלוח (ראשון עד חמישי)
+// ימים שמוצגים בלוח
 export const HE_DAYS = [
   "ראשון",
   "שני",
@@ -16,43 +16,38 @@ export const HE_DAYS = [
 ];
 
 
-// סלוטים עגולים בלבד (שעות פעילות)
+// סלוטים עגולים בלבד
 export const TIMESLOTS = [
-  "08:00",
-  "09:00",
-  "10:00",
-  "11:00",
-  "12:00",
-  "13:00",
-  "14:00",
-  "15:00",
-  "16:00"
+  "08:00-09:00",
+  "09:00-10:00",
+  "10:00-11:00",
+  "11:00-12:00",
+  "12:00-13:00",
+  "13:00-14:00",
+  "14:00-15:00",
+  "15:00-16:00"
 ];
 
 
-// חשבונות זום זמינים במערכת
+// חשבונות זום
 export const ZOOM_ACCOUNTS = [
-  {
-    key: "Z1",
-    name: "Zoom 1",
-    joinUrl: "https://zoom.us/j/6023602336?omn=96962875568"
-  },
-  {
-    key: "Z2",
-    name: "Zoom 2",
-    joinUrl: "https://zoom.us/j/7601360450?omn=98989531483"
-  },
-  {
-    key: "Z3",
-    name: "Zoom 3",
-    joinUrl: "https://zoom.us/j/5274325600?omn=96368524491"
-  }
+  "Zoom1",
+  "Zoom2",
+  "Zoom3"
 ];
 
 
-// כמה דקות לפני תחילת שיעור השיבוץ ננעל
-export const LOCK_MINUTES_BEFORE_START = 5;
+// קישורי זום קבועים
+export const ZOOM_LINKS = {
+  Zoom1: "https://zoom.us/j/6023602336?omn=96962875568",
+  Zoom2: "https://zoom.us/j/7601360450?omn=98989531483",
+  Zoom3: "https://zoom.us/j/5274325600?omn=96368524491"
+};
 
 
-// הגדרת אזור זמן
+// זמן מוגן לפני שיעור (בשעות)
+export const PROTECTED_HOURS = 0;
+
+
+// אזור זמן
 export const TIMEZONE = "Asia/Jerusalem";
